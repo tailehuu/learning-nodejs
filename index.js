ae8ee4033d32e324c26d88cdd2d87817b74630a0
Tai LE <lhtai181@gmail.com>
2022-09-05 17:54:39 +0700

    minimist

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const fs = require('fs')
+const Path = require('path')
+const process = require('process')
 
-console.log('hi')
+// node index.js --date=2022-04-18 --token=BTC
+const argv = require('minimist')(process.argv.slice(2))
+console.log('argv: ', argv)
+console.log('date: ', argv.date)
+console.log('token: ', argv.token)
+
